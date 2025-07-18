8dccfc95674bd247c1ddaa5768b8197c01160645
Mark Nguyen <khoi_nguyen_hao@bita.jp>
2025-07-18 11:16:59 +0700

    Kuroco frontend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  runtimeConfig: {
+      // Public keys that are exposed to the client
+      public: {
+          apiBase: 'https://marktest.g.kuroco.app'
+      }
+  },
 })
+
+
